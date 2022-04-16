f01bed713238fcb4f4bc53d04fa05e3ca99bef97
Kenan Mursalov <kenan.mursalov@gmail.com>
2022-04-16 18:29:55 +0300

    Solved warnings

--- car-simulator.loc/Classes/MovableInterface.php
+++ car-simulator.loc/Classes/MovableInterface.php
@@ -8,27 +8,27 @@ interface MovableInterface
      * Включает зажигание
      * @return mixed
      */
-    public function start(): mixed;
+    public function start();
 
     /**
      * Выключает зажигание
      * @return mixed
      */
-    public function stop(): mixed;
+    public function stop();
 
     /**
      * Увеличивает скорость движения
      * @param int $unit
      * @return mixed
      */
-    public function up(int $unit): mixed;
+    public function up(int $unit);
 
     /**
      * Уменьшает скорость движения
      * @param int $unit
      * @return mixed
      */
-    public function down(int $unit): mixed;
+    public function down(int $unit);
 
     public function getName(): string;
 }
